232a5d6d48e5e5efc5dc92293c6302c6d3f548f6
Musa <mangabomoses@gmail.com>
2024-03-07 14:47:33 +0300

    Initializing...

---
+++ src/main/java/engmusa/BankingSystemApplication.java
@@ -0,0 +1,13 @@
+package engmusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemApplication.class, args);
+	}
+
+}
