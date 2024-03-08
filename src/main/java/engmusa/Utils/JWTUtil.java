0a5bdac9d3eeaea855c0fea3a8bddf660015d983
Musa <mangabomoses@gmail.com>
2024-03-08 11:06:41 +0300

    JWT Util

---
+++ src/main/java/engmusa/Utils/JWTUtil.java
@@ -0,0 +1,49 @@
+package engmusa.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+public class JWTUtil {
+
+    private Key getSignInKey(){
+        byte[] key= Decoders.BASE64.decode("a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2");
+        return Keys.hmacShaKeyFor(key);
+    }
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*1000*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+    private boolean isTokenExpired(String token){
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+}
